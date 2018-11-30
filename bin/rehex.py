f3fcdd28db66ecef7bc04974c3b09ed94d73cc17
Developer <44878271+advantage-development@users.noreply.github.com>
2018-11-30 10:43:04 -0600

    Update rehex.py

--- bin/rehex.py
+++ bin/rehex.py
@@ -6,7 +6,7 @@ from pprint import pprint
 import sys
 import os
 sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../lib')))
-import anonlib
+import advantagelib
 # ============================================================================
 usage = "%s <hex>" % sys.argv[0]
 
